22640be95c2d556d60b3d268fdcccc2a0d09fe64
Tatyana-Kornienko <tatyana.kornienko.40@gmail.com>
2020-08-13 02:21:40 +0300

    Zavdannia_9

---
+++ Zavdannia_9/Zavdannia_9.js
@@ -0,0 +1,279 @@
+var groupNumber1 = [{
+    groupId: 1,
+    id: 1,
+    name: "Duke Price",
+    mark: 91,
+    science: "Algebra",
+    photo: "https://99px.ru/sstorage/1/2020/08/image_10608201031553430081.gif"
+},{
+    groupId: 1,
+    id: 2,
+    name: "Quintus Roberts",
+    mark:13,
+    science: "Geometry ",
+    photo: "https://99px.ru/sstorage/1/2020/08/image_10608201031138976370.gif"
+},{
+    groupId: 1,
+    id: 3,
+    name: "Bristol Henderson",
+    mark: 28,
+    science: "Algebra",
+    photo: "https://99px.ru/sstorage/1/2020/08/image_10408201858007177270.gif"
+},{
+    groupId: 1,
+    id: 4,
+    name: "Quennel Martin",
+    mark: 54,
+    science: "Chemistry",
+    photo: "https://99px.ru/sstorage/1/2020/08/image_10408201856232786430.gif"
+},{
+    groupId: 1,
+    id: 5,
+    name: "Vinson Martinez",
+    mark: 67,
+    science: "Algebra",
+    photo: "https://99px.ru/sstorage/1/2020/08/image_10208200008179892735.gif"
+},{
+    groupId: 1,
+    id: 6,
+    name: "Marcus Smith",
+    mark: 21,
+    science: "History",
+    photo: "https://99px.ru/sstorage/1/2020/08/image_10208200007554360938.gif"
+},{
+    groupId: 1,
+    id: 7,
+    name: "Lukas Morris",
+    mark: 93,
+    science: "Literature",
+    photo: "https://99px.ru/sstorage/1/2020/08/image_10208200007194039621.gif"
+},{
+    groupId: 1,
+    id: 8,
+    name: "Vladimir Coleman",
+    mark: 89,
+    science: "Geometry ",
+    photo: "https://99px.ru/sstorage/1/2020/07/image_13107202020172586623.gif"
+},{
+    groupId: 1,
+    id: 9,
+    name: "Brentley Foster",
+    mark: 100,
+    science: "Chemistry",
+    photo: "https://99px.ru/sstorage/1/2020/07/image_13007200006552599710.gif"
+},{
+    groupId: 1,
+    id: 10,
+    name: "Natan Parker",
+    mark: 4,
+    science: "Geometry ",
+    photo: "https://99px.ru/sstorage/1/2020/07/image_12907201952098683118.gif"
+}];
+
+var groupNumber2 = [
+{
+    groupId: 2,
+    id: 1,
+    name: "Drake Turner",
+    mark: 95,
+    science: "Algebra",
+    photo: "https://99px.ru/sstorage/1/2020/07/image_12907201951096095902.gif"
+},{
+    groupId: 2,
+    id: 2,
+    name: "Matias Cook",
+    mark: 85,
+    science: "Geometry ",
+    photo: "https://99px.ru/sstorage/1/2020/07/image_12807200024179124717.gif"
+},{
+    groupId: 2,
+    id: 3,
+    name: "Kingsley Green",
+    mark: 76,
+    science: "Algebra",
+    photo: "https://99px.ru/sstorage/1/2020/07/image_12807200023152149690.gif"
+},{
+    groupId: 2,
+    id: 4,
+    name: "Zain Young",
+    mark: 100,
+    science: "Chemistry",
+    photo: "https://99px.ru/sstorage/1/2020/07/image_12707200200547490920.gif"
+},{
+    groupId: 2,
+    id: 5,
+    name: "Kenley Watson",
+    mark: 68,
+    science: "Algebra",
+    photo: "https://99px.ru/sstorage/1/2020/07/image_12607201834438898492.gif"
+},{
+    groupId: 2,
+    id: 6,
+    name: "Pierson Ramirez",
+    mark: 73,
+    science: "History",
+    photo: "https://99px.ru/sstorage/1/2020/07/image_12607201833403697297.gif"
+},{
+    groupId: 2,
+    id: 7,
+    name: "Uzziel Roberts",
+    mark: 77,
+    science: "Literature",
+    photo: "https://99px.ru/sstorage/1/2020/07/image_11707200140308109719.gif"
+},{
+    groupId: 2,
+    id: 8,
+    name: "Valente Walker",
+    mark: 58,
+    science: "Geometry ",
+    photo: "https://99px.ru/sstorage/1/2020/07/image_11607201632445786620.gif"
+},{
+    groupId: 2,
+    id: 9,
+    name: "Lorenzo Young",
+    mark: 23,
+    science: "Chemistry",
+    photo: "https://99px.ru/sstorage/1/2020/07/image_11507200122468329236.gif"
+},{
+    groupId: 2,
+    id: 10,
+    name: "Immanuel Wright",
+    mark: 96,
+    science: "Geometry ",
+    photo: "https://99px.ru/sstorage/1/2020/07/image_1140720193148969713.gif"
+}
+];
+
+var groupNumber3 = [
+    {
+        groupId: 3,
+        id: 1,
+        name: "Nestor Miller",
+        mark: 55,
+        science: "Algebra",
+        photo: "https://99px.ru/sstorage/1/2020/07/image_11307202031484371344.gif"
+    },{
+        groupId: 3,
+        id: 2,
+        name: "Maverick Phillips",
+        mark: 52,
+        science: "Geometry ",
+        photo: "https://99px.ru/sstorage/1/2020/07/image_11307202031142998118.gif"
+    },{
+        groupId: 3,
+        id: 3,
+        name: "Noam Thompson",
+        mark: 68,
+        science: "Algebra",
+        photo: "https://99px.ru/sstorage/1/2020/07/image_1130720203028574758.gif"
+    },{
+        groupId: 3,
+        id: 100,
+        name: "Tanner Price",
+        mark: 97,
+        science: "Chemistry",
+        photo: "https://99px.ru/sstorage/1/2020/07/image_106072020344359209.gif"
+    },{
+        groupId: 3,
+        id: 5,
+        name: "Cullen Ramirez",
+        mark: 13,
+        science: "Algebra",
+        photo: "https://99px.ru/sstorage/1/2020/07/image_10507201947421711035.gif"
+    },{
+        groupId: 3,
+        id: 6,
+        name: "Case Sanders",
+        mark: 92,
+        science: "History",
+        photo: "https://99px.ru/sstorage/1/2020/06/image_13006201627556536619.gif"
+    },{
+        groupId: 3,
+        id: 7,
+        name: "Conor Jenkins",
+        mark: 97,
+        science: "Literature",
+        photo: "https://99px.ru/sstorage/1/2020/06/image_12906200109098069188.gif"
+    },{
+        groupId: 3,
+        id: 8,
+        name: "Dean Miller",
+        mark: 75,
+        science: "Geometry ",
+        photo: "https://99px.ru/sstorage/1/2020/06/image_1250620132830839813.gif"
+    },{
+        groupId: 3,
+        id: 9,
+        name: "Vinson Long",
+        mark: 90,
+        science: "Chemistry",
+        photo: "https://99px.ru/sstorage/1/2020/06/image_11506201239454516974.gif"
+    },{
+        groupId: 3,
+        id: 10,
+        name: "Jose Bennett",
+        mark: 45,
+        science: "Geometry ",
+        photo: "https://99px.ru/sstorage/1/2020/06/image_12106200840108917326.gif"
+    }
+    ];
+
+
+
+allStudentMarks(groupNumber1, groupNumber2, groupNumber3 );
+
+function allStudentMarks(){
+   	var allMarks = [].concat(...arguments);
+    var levelStudents = sortMarks( allMarks);
+    displayResults(levelStudents);
+}
+
+function sortMarks(data){
+	var highLevel=[], middleLevel = [], lowLevel= [];
+	for(let studentData of data){
+        if(studentData.mark >= 89){
+            levelStudents(studentData, highLevel)  
+        }else if(studentData.mark >= 65 && studentData.mark < 89){
+            levelStudents(studentData, middleLevel) 
+        }else{
+            levelStudents(studentData, lowLevel) 
+        }
+    }
+    return [].concat([highLevel], [middleLevel], [lowLevel]) 
+}
+
+function levelStudents(studentData, array){
+  return array.push(studentData)
+}
+
+function displayResults(levelStudents){
+    var studentsList = document.querySelector("#students-list");
+	for(let i = 0; i < levelStudents.length; i++){
+        var blockHeader =  createElement("h1", "user-header", `<br> ${
+            (i == 0)? "High Level" :
+            (i == 1)? "Middle Level" : "Low Level"}`)
+        
+        let classDisplay = (i == 0)? "top-group" : (i == 1)? "middle-group" : "bottom-group";
+        studentsList.appendChild(blockHeader);
+        for(let j = 0; j < levelStudents[i].length; j++){
+            let studentsDataFlow = levelStudents[i][j]
+            let innerText = `<div>
+                    ${studentsDataFlow.name} group number ${studentsDataFlow.groupId} 
+                    <img src="${studentsDataFlow.photo}"> <mark>${studentsDataFlow.mark}</mark> 
+                    <span>${studentsDataFlow.science}</span>        
+                </div>`;
+            let newStudentElement = createElement("li", `student-card ${classDisplay}`, innerText);
+            studentsList.appendChild(newStudentElement);
+        }
+        
+    }
+}
+
+
+function createElement(tag, className, innerText){
+    var newObject = document.createElement(tag);
+    newObject.className = (className)? className : false;
+    newObject.innerHTML = (innerText)? innerText : false;
+
+    return newObject
+}
